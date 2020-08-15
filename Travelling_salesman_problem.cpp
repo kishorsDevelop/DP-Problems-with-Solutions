8cca0699a152ba780993349ff8c64594d1e081e4
kishor kumar <33126255+kishorsDevelop@users.noreply.github.com>
2020-08-15 17:06:59 +0530

    Update Travelling_salesman_problem.cpp

--- Travelling_salesman_problem.cpp
+++ Travelling_salesman_problem.cpp
@@ -8,7 +8,7 @@ int dist[10][10] = {
                      {15,35,0,30},
                      {20,25,30,0}
                    };
-int VISITED = (1<<n)-1;
+int VISITED = (1<<n)-1;    // using bit  masking to mark every visited ans non visited city
 int tsp(int mask,int prevCity){
 	 if(mask == VISITED){
 	 	return dist[prevCity][0];
@@ -17,12 +17,13 @@ int tsp(int mask,int prevCity){
 	 	  return dp[mask][prevCity];
 	 }
      int ans = INT_MAX;
+	
 	 for(int newCity = 0; newCity < n; newCity++){
 	 	   if((mask & (1<<newCity)) == 0){
-                 int minimum = dist[prevCity][newCity] + 
-                               tsp(mask|(1<<newCity),newCity);
+			   
+                      int minimum = dist[prevCity][newCity] +  tsp(mask|(1<<newCity),newCity);
 
-                 ans = min(ans,minimum);
+                         ans = min(ans,minimum);
 	 	   }
 	 }
 	 return dp[mask][prevCity] = ans;
