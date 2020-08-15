b754e742926258a1140f62445cd62da897dba817
kishor kumar <33126255+kishorsDevelop@users.noreply.github.com>
2020-08-15 17:05:29 +0530

    Create Travelling_salesman_problem.cpp

---
+++ Travelling_salesman_problem.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dp[100][10];
+int n = 4;
+int dist[10][10] = {
+                     {0,10,15,20},
+                     {10,0,35,25},
+                     {15,35,0,30},
+                     {20,25,30,0}
+                   };
+int VISITED = (1<<n)-1;
+int tsp(int mask,int prevCity){
+	 if(mask == VISITED){
+	 	return dist[prevCity][0];
+	 }
+	 if(dp[mask][prevCity] != -1){
+	 	  return dp[mask][prevCity];
+	 }
+     int ans = INT_MAX;
+	 for(int newCity = 0; newCity < n; newCity++){
+	 	   if((mask & (1<<newCity)) == 0){
+                 int minimum = dist[prevCity][newCity] + 
+                               tsp(mask|(1<<newCity),newCity);
+
+                 ans = min(ans,minimum);
+	 	   }
+	 }
+	 return dp[mask][prevCity] = ans;
+}
+int main()
+{
+	memset(dp,-1,sizeof(dp));
+   cout<<tsp(1,0)<<"\n";
+}
